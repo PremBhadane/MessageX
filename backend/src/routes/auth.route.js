10f088cb5d524a0554b1b1a00c1030523111e994
PremBhadane <prembhadane69@gmail.com>
2026-03-11 22:43:49 +0530

    sidebar completed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -11,6 +11,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
-router.get("/check", protectRoute, (req, res) => res.send(200).json(req.user));
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
 export default router;
