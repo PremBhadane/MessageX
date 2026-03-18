4592407325ab390d888dbfa43a26b2a47eec336d
PremBhadane <prembhadane69@gmail.com>
2026-03-18 18:26:08 +0530

    fix: use backend URL in production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api` : "/api",
+    // baseURL: import.meta.env.MODE === "development" ? `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api` : "/api",
+    baseURL: `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api`,
     withCredentials: true,
 });
