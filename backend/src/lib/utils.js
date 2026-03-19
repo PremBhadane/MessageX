c20837c742e3bba42bbedd58d627d61cf0376b92
PremBhadane <prembhadane69@gmail.com>
2026-03-19 15:19:49 +0530

    fix: sameSite none for cross-domain cookies in production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,7 +14,8 @@ export const generateToken = (userId, res) =>{
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, //Mili Seconds
         httpOnly: true,      // prevent XSS attacks: cross-site scripting
-        sameSite: "strict",  // CSRF attacks
+        // sameSite: "strict",  // CSRF attacks
+        sameSite: ENV.NODE_ENV === "development" ? "strict" : "none", // ← Fix
         secure: ENV.NODE_ENV === "development" ? false : true,
     });
 
